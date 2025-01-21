c6aaf1473c9da67f819b03dbd4a487b989e73587
VishvijayPS <vishvijayps123@mail.com>
2025-01-21 22:41:12 +0530

    updating Decrypt.py

--- Decrypt.java
+++ Decrypt.java
@@ -87,84 +87,32 @@ public class Decrypt {
         char result = 0;
         
         switch (number) {
-            case 997:
-                result = 'a';
-                break;
-            case 995:
-                result = 'b';
-                break;
-            case 993:
-                result = 'c';
-                break;
-            case 991:
-                result = 'd';
-                break;
-            case 989:
-                result = 'e';
-                break;
-            case 987:
-                result = 'f';
-                break;
-            case 985:
-                result = 'g';
-                break;
-            case 983:
-                result = 'h';
-                break;
-            case 981:
-                result = 'i';
-                break;
-            case 979:
-                result = 'j';
-                break;
-            case 977:
-                result = 'k';
-                break;
-            case 975:
-                result = 'l';
-                break;
-            case 973:
-                result = 'm';
-                break;
-            case 971:
-                result = 'n';
-                break;
-            case 969:
-                result = 'o';
-                break;
-            case 967:
-                result = 'p';
-                break;
-            case 965:
-                result = 'q';
-                break;
-            case 963:
-                result = 'r';
-                break;
-            case 961:
-                result = 's';
-                break;
-            case 959:
-                result = 't';
-                break;
-            case 957:
-                result = 'u';
-                break;
-            case 955:
-                result = 'v';
-                break;
-            case 953:
-                result = 'w';
-                break;
-            case 951:
-                result = 'x';
-                break;
-            case 949:
-                result = 'y';
-                break;
-            case 947:
-                result = 'z';
-                break;
+            case 997 -> result = 'a';
+            case 995 -> result = 'b';
+            case 993 -> result = 'c';
+            case 991 -> result = 'd';
+            case 989 -> result = 'e';
+            case 987 -> result = 'f';
+            case 985 -> result = 'g';
+            case 983 -> result = 'h';
+            case 981 -> result = 'i';
+            case 979 -> result = 'j';
+            case 977 -> result = 'k';
+            case 975 -> result = 'l';
+            case 973 -> result = 'm';
+            case 971 -> result = 'n';
+            case 969 -> result = 'o';
+            case 967 -> result = 'p';
+            case 965 -> result = 'q';
+            case 963 -> result = 'r';
+            case 961 -> result = 's';
+            case 959 -> result = 't';
+            case 957 -> result = 'u';
+            case 955 -> result = 'v';
+            case 953 -> result = 'w';
+            case 951 -> result = 'x';
+            case 949 -> result = 'y';
+            case 947 -> result = 'z';
         }
 
         return result;
@@ -174,84 +122,32 @@ public class Decrypt {
         char result = 0;
         
         switch (number) {
-            case 998:
-                result = 'A';
-                break;
-            case 996:
-                result = 'B';
-                break;
-            case 994:
-                result = 'C';
-                break;
-            case 992:
-                result = 'D';
-                break;
-            case 990:
-                result = 'E';
-                break;
-            case 988:
-                result = 'F';
-                break;
-            case 986:
-                result = 'G';
-                break;
-            case 984:
-                result = 'H';
-                break;
-            case 982:
-                result = 'I';
-                break;
-            case 980:
-                result = 'J';
-                break;
-            case 978:
-                result = 'K';
-                break;
-            case 976:
-                result = 'L';
-                break;
-            case 974:
-                result = 'M';
-                break;
-            case 972:
-                result = 'N';
-                break;
-            case 970:
-                result = 'O';
-                break;
-            case 968:
-                result = 'P';
-                break;
-            case 966:
-                result = 'Q';
-                break;
-            case 964:
-                result = 'R';
-                break;
-            case 962:
-                result = 'S';
-                break;
-            case 960:
-                result = 'T';
-                break;
-            case 958:
-                result = 'U';
-                break;
-            case 956:
-                result = 'V';
-                break;
-            case 954:
-                result = 'W';
-                break;
-            case 952:
-                result = 'X';
-                break;
-            case 950:
-                result = 'Y';
-                break;
-            case 948:
-                result = 'Z';
-                break;
+            case 998 -> result = 'A';
+            case 996 -> result = 'B';
+            case 994 -> result = 'C';
+            case 992 -> result = 'D';
+            case 990 -> result = 'E';
+            case 988 -> result = 'F';
+            case 986 -> result = 'G';
+            case 984 -> result = 'H';
+            case 982 -> result = 'I';
+            case 980 -> result = 'J';
+            case 978 -> result = 'K';
+            case 976 -> result = 'L';
+            case 974 -> result = 'M';
+            case 972 -> result = 'N';
+            case 970 -> result = 'O';
+            case 968 -> result = 'P';
+            case 966 -> result = 'Q';
+            case 964 -> result = 'R';
+            case 962 -> result = 'S';
+            case 960 -> result = 'T';
+            case 958 -> result = 'U';
+            case 956 -> result = 'V';
+            case 954 -> result = 'W';
+            case 952 -> result = 'X';
+            case 950 -> result = 'Y';
+            case 948 -> result = 'Z';
         }
 
         return result;
@@ -261,102 +157,38 @@ public class Decrypt {
         char result = 0;
         
         switch (number) {
-            case 697:
-                result = '`';
-                break;
-            case 694:
-                result = '~';
-                break;
-            case 691:
-                result = '!';
-                break;
-            case 688:
-                result = '@';
-                break;
-            case 685:
-                result = '#';
-                break;
-            case 682:
-                result = '$';
-                break;
-            case 679:
-                result = '%';
-                break;
-            case 676:
-                result = '^';
-                break;
-            case 673:
-                result = '&';
-                break;
-            case 670:
-                result = '*';
-                break;
-            case 667:
-                result = '(';
-                break;
-            case 664:
-                result = ')';
-                break;
-            case 661:
-                result = '-';
-                break;
-            case 658:
-                result = '_';
-                break;
-            case 655:
-                result = '=';
-                break;
-            case 652:
-                result = '+';
-                break;
-            case 649:
-                result = ';';
-                break;
-            case 646:
-                result = ':';
-                break;
-            case 643:
-                result = '{';
-                break;
-            case 640:
-                result = '}';
-                break;
-            case 637:
-                result = '[';
-                break;
-            case 634:
-                result = ']';
-                break;
-            case 631:
-                result = '|';
-                break;
-            case 628:
-                result = '\\';
-                break;
-            case 625:
-                result = '<';
-                break;
-            case 622:
-                result = ',';
-                break;
-            case 619:
-                result = '>';
-                break;
-            case 616:
-                result = '.';
-                break;
-            case 613:
-                result = '/';
-                break;
-            case 610:
-                result = '?';
-                break;
-            case 607:
-                result = '"';
-                break;
-            case 604:
-                result = '\'';
-                break;
+            case 697 -> result = '`';
+            case 694 -> result = '~';
+            case 691 -> result = '!';
+            case 688 -> result = '@';
+            case 685 -> result = '#';
+            case 682 -> result = '$';
+            case 679 -> result = '%';
+            case 676 -> result = '^';
+            case 673 -> result = '&';
+            case 670 -> result = '*';
+            case 667 -> result = '(';
+            case 664 -> result = ')';
+            case 661 -> result = '-';
+            case 658 -> result = '_';
+            case 655 -> result = '=';
+            case 652 -> result = '+';
+            case 649 -> result = ';';
+            case 646 -> result = ':';
+            case 643 -> result = '{';
+            case 640 -> result = '}';
+            case 637 -> result = '[';
+            case 634 -> result = ']';
+            case 631 -> result = '|';
+            case 628 -> result = '\\';
+            case 625 -> result = '<';
+            case 622 -> result = ',';
+            case 619 -> result = '>';
+            case 616 -> result = '.';
+            case 613 -> result = '/';
+            case 610 -> result = '?';
+            case 607 -> result = '"';
+            case 604 -> result = '\'';
         }
 
         return result;
@@ -366,36 +198,16 @@ public class Decrypt {
         char result = 0;
 
         switch (number) {
-            case 123:
-                result = '1';
-                break;
-            case 126:
-                result = '2';
-                break;
-            case 129:
-                result = '3';
-                break;
-            case 132:
-                result = '4';
-                break;
-            case 135:
-                result = '5';
-                break;
-            case 138:
-                result = '6';
-                break;
-            case 141:
-                result = '7';
-                break;
-            case 144:
-                result = '8';
-                break;
-            case 147:
-                result = '9';
-                break;
-            case 150:
-                result = '0';
-                break;
+            case 123 -> result = '1';
+            case 126 -> result = '2';
+            case 129 -> result = '3';
+            case 132 -> result = '4';
+            case 135 -> result = '5';
+            case 138 -> result = '6';
+            case 141 -> result = '7';
+            case 144 -> result = '8';
+            case 147 -> result = '9';
+            case 150 -> result = '0';
         }
 
         return result;
